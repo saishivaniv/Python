36cec6a32e0ebdc600f0b259b8605d8f7b31aed0
saishivaniv <saishivani.v@gmail.com>
2026-08-31 01:19:21 +0530

    add print and variables

---
+++ basics/print.py
@@ -0,0 +1,47 @@
+# Comments - a note in the code
+# This is a single-line comment
+# Inline comment - at end of the code
+x = 6 # exam score
+# this multi-line comment 
+# 1.Create user data
+# 2.Add details of user
+
+# --------------------------------------------------------------------------
+#print() function
+# built-in python function that displays message on the output screen to communicate with users
+
+print("Python programming")
+print('python')
+# print("hi')
+print("------------------------------")
+print('PYTHON')
+print("------------------------------")
+
+
+# Escape Sequence - special characters
+# \" - double quote
+# \' - single quote
+# \\ - backslash
+# \n - new line
+# \t - tab
+# \b - backspace
+
+print("ESCAPE SEQUENCES")
+print("----------------------------")
+print("python \"programming\"")
+print('python \'programming\'')
+print("path : Desktop\\users")
+print("message1 \n message 2")
+print("message1\tmessage 2")
+
+print("Learning path :\n\t- python\n\t- data analyst\n\t- ai")
+
+
+# Triple quotes - allows us to write text in multiple strings
+
+print("""Learning path :
+\t- python
+\t- data analyst
+\t- ai""")
+
+
---
+++ basics/variables.py
@@ -0,0 +1,14 @@
+# Variables
+# It is a name you create to store a value, in order to use it later in your code
+
+name = "maria"
+print(name,"learning python")
+print(name,"wants to become python expert")
+
+language = "Python"
+print(name,"learning", language)
+print(name,"wants to become", language, "expert")
+
+
+info = "info@maria.com"
+print("Contact",info)
