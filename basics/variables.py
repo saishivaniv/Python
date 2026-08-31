db671dd568550c1fea99c376cae1c0be204e573b
saishivaniv <saishivani.v@gmail.com>
2026-09-01 03:54:20 +0530

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -12,3 +12,18 @@ print(name,"wants to become", language, "expert")
 
 info = "info@maria.com"
 print("Contact",info)
+
+
+
+
+# input() - get something from the users   built-in function 
+input("enter your name") #reads the user's response but immediately discards
+name = input("Enter your Name:")
+print("You are", name)
+
+# Hard Coded(Static)Values - fixed piece of data written directly into your code that never changesat runtime.
+# Dynamic Values - data entered by the user that can vary each time the program runs. 
+
+name = input("Enter your Name:")
+country = "India"
+print(name, "comes from", country)
